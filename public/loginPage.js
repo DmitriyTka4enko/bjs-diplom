7f16a813072340212b70aac0391b02ad3f7791c5
Dmitriy Tkachenko <Tkachenko_D_V@mail.ru>
2023-11-21 15:17:37 +0900

    loginPage corrected

--- public/loginPage.js
+++ public/loginPage.js
@@ -2,19 +2,19 @@
 
 let userForm = new UserForm();
 
-userForm.loginFormCallback = (data) => {
+userForm.loginFormCallback = data => {
 	ApiConnector.login(data, response => {
-		if (response.succes === true) {
+		if (response.success) {
 			location.reload();
 		} else {
 			userForm.setLoginErrorMessage('Ошибка при авторизации: ' + response.error);
 		}
 	});
-};
+}
 
-userForm.registerFormCallback = (data) => {
+userForm.registerFormCallback = data => {
 	ApiConnector.register(data, response => {
-		if (response.succes === true) {
+		if (response.success) {
 			location.reload();
 		} else {
 			userForm.setRegisterErrorMessage('Ошибка при регистрации: ' + response.error);
